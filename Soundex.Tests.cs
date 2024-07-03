7d27fd5f666aa721e7a84f160dbdb766507b7967
navanee867 <navanee777@gmail.com>
2024-07-03 19:17:32 +0530

    Update Soundex.Tests.cs

--- Soundex.Tests.cs
+++ Soundex.Tests.cs
@@ -13,6 +13,73 @@ public class SoundexTests
     {
         Assert.Equal("A000", Soundex.GenerateSoundex("A"));
     }
+    [Fact]
+    public void HandlesSimpleName()
+    {
+        Assert.Equal("N153", Soundex.GenerateSoundex("Navaneeth"));
+    }
+    [Fact]
+    public void HandlesNameWithDuplicates()
+    {
+        Assert.Equal("W252", Soundex.GenerateSoundex("Washington"));
+    }
+        [Fact]
+    public void HandlesNameWithVowels()
+    {
+        Assert.Equal("R163", Soundex.GenerateSoundex("Robert"));
+    }
 
-   
+    [Fact]
+    public void HandlesNameWithSilentLetters()
+    {
+        Assert.Equal("P260", Soundex.GenerateSoundex("Pfiser"));
+    }
+
+    [Fact]
+    public void HandlesUppercaseAndLowercase()
+    {
+        Assert.Equal(Soundex.GenerateSoundex("Lee"), Soundex.GenerateSoundex("LEE"));
+    }
+
+    [Fact]
+    public void HandlesNameEndingInS()
+    {
+        Assert.Equal("S520", Soundex.GenerateSoundex("Sanders"));
+    }
+
+    [Fact]
+    public void HandlesNameWithConsecutiveSameCodes()
+    {
+        Assert.Equal("T523", Soundex.GenerateSoundex("Tymczak"));
+    }
+
+    [Fact]
+    public void HandlesNameWithNoConsonants()
+    {
+        Assert.Equal("A000", Soundex.GenerateSoundex("Aeo"));
+    }
+
+    [Fact]
+    public void HandlesVeryLongName()
+    {
+        Assert.Equal("H250", Soundex.GenerateSoundex("Hernandez"));
+    }
+
+    [Fact]
+    public void HandlesMixedCaseName()
+    {
+        Assert.Equal("A123", Soundex.GenerateSoundex("Ameba"));
+    }
+
+    [Fact]
+    public void HandlesNameWithSpaces()
+    {
+        Assert.Equal("D520", Soundex.GenerateSoundex("De La Cruz"));
+    }
+
+    [Fact]
+    public void HandlesHyphenatedName()
+    {
+        Assert.Equal("M620", Soundex.GenerateSoundex("Mac-Dowell"));
+    }
 }
